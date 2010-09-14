393825c61aeaf5ea20aba18ba70b41c079e79c8e
matt beldyk <beldyk@Sloth.(none)>
2010-09-13 20:54:56 -0600

    cleanup experimental garbage

--- csci5722_cv/assignment_1/cat_assign.m
+++ csci5722_cv/assignment_1/cat_assign.m
@@ -1,17 +1,12 @@
 function [] = cat_assign()
 path = '/home/beldyk/Desktop/cat';
-%i = 1;
-%img_file = strcat(path, sprintf('/OBJ_%02d.png',i));
-    
-%cat_pic = imread(img_file)
-%size(cat_pic)
-%kitten= zeros(13,size(cat_pic));
-%size(kitten)
-%kitten(1,:,:,:) = cat_pic;
-%cat_pics =[];
+
+% Allocate space for files
 cat_pics = zeros(247,220,3,13);
 matte_pics = zeros(128,109,3,13);
 shiny_pics = zeros(128,109,3,13);
+
+% read in files
 for i = 1:13
 
     cat_file = strcat(path, sprintf('/OBJ_%02d.png',i));
@@ -29,6 +24,8 @@ pic = cat_pics(:,:,:,6)/255.;
 %pixle_space = cat_pics(100,100,:,:)
 pic_size = size(pic);
 image(pic);
+
+
 
 
 
