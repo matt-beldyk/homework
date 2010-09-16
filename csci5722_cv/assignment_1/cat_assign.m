9bd3b3ac231228627c031c057b411ceda628b0a3
matt beldyk <beldyk@Sloth.(none)>
2010-09-15 20:41:52 -0600

    code cleanup

--- csci5722_cv/assignment_1/cat_assign.m
+++ csci5722_cv/assignment_1/cat_assign.m
@@ -24,42 +24,35 @@ for i = 1:img_count
     matte_pics(:,:,:,i) = imread(matte_file);
     shiny_pics(:,:,:,i) = imread(shiny_file);
     
-   % kitten(i) = cat_pic;
+  
 end
 ref_mask = imread(strcat(path, '/spheremask.png'));
 cat_mask = imread(strcat(path, '/catmask.png'));
 
 invs = get_ref_invs(matte_pics, shiny_pics, ref_mask);
 
-%size(cat_pics)
-%pic = cat_pics(:,:,:,6)/255.;
-%pixle_space = cat_pics(100,100,:,:)
-%pic_size = size(pic);
-%image(pic);
-shiny_mapping = zeros(cat_h, cat_w, 2);
-matte_mapping = zeros(cat_h, cat_w, 2);
-%for x = 1:cat_h
-   
+
+mapping = zeros(cat_h, cat_w, 2);
+
+
+%for x = 1:cat_h   
 for x = 1:30
     x
     for y = 1:cat_w
         if is_masked(cat_mask, x, y)
             
             cat_vect = reshape(cat_pics(x,y,:,:),39,1);
-           % [best_sx, best_sy] = find_closest_loc(shiny_pics, ref_mask, cat_vect, 128, 109);
-          %  shiny_mapping(x,y,:) = [best_sx, best_sy];
-        
             [best_mx, best_my] = find_closest_loc(invs, ref_mask, cat_vect, 128, 109);
-            matte_mapping(x,y,:) = [best_mx, best_my];
+            mapping(x,y,:) = [best_mx, best_my];
         end       
     end
 end
 
 matte_cat = zeros(cat_h,cat_w,3);
-for x = 1:247
-    for y = 1:220
-        bx = matte_mapping(x,y,1);
-        by = matte_mapping(x,y,2);
+for x = 1:cat_h
+    for y = 1:cat_w
+        bx = mapping(x,y,1);
+        by = mapping(x,y,2);
         if bx && by
             matte_vect = matte_pics(bx,by,:,1);
             matte_cat(x,y,:) = matte_vect/255;
