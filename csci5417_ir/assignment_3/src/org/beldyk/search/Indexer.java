7222b3145d3cb4339c8fdc881111c9ba9143de2f
matt beldyk <beldyk@Sloth.(none)>
2010-09-16 23:00:06 -0600

    initial import

---
+++ csci5417_ir/assignment_3/src/org/beldyk/search/Indexer.java
@@ -0,0 +1,58 @@
+package org.beldyk.search;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Collection;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.index.CorruptIndexException;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.store.LockObtainFailedException;
+import org.apache.lucene.util.Version;
+
+public class Indexer {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 * @throws LockObtainFailedException 
+	 * @throws CorruptIndexException 
+	 */
+	public static void main(String[] args) throws CorruptIndexException, LockObtainFailedException, IOException {
+		// TODO Auto-generated method stub
+
+		File indexDir = new File("/tmp/lu_index/");
+		String documents = "/home/beldyk/Desktop/medical.txt";
+		//String documents = "/home/beldyk/Desktop/short_med.txt";
+		index(indexDir, documents);
+
+	}
+
+	public static void index(File indexDir, String documents) throws CorruptIndexException, LockObtainFailedException, IOException{
+		DocuReader rdr = new DocuReader();
+		IndexWriter wr = new IndexWriter(FSDirectory.open(indexDir),  
+				new StandardAnalyzer(Version.LUCENE_CURRENT), 
+				true, IndexWriter.MaxFieldLength.LIMITED);
+
+		//			indexDir, new StandardAnalyzer(), true);
+
+		try {
+			Collection<MedDoc> docs = rdr.read(documents);
+			for(MedDoc d: docs){
+				Document doc = new Document();
+				for(String a:d.keySet()){
+					doc.add(new Field(a, d.get(a), Field.Store.YES, Field.Index.ANALYZED));
+				}
+				wr.addDocument(doc);
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ csci5417_ir/assignment_3/src/org/beldyk/search/MedDoc.java
@@ -0,0 +1,108 @@
+package org.beldyk.search;
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class MedDoc extends HashMap<String,String>{
+	/*
+	.I 1
+	.U
+	87049087
+	.S
+	Am J Emerg Med 8703; 4(6):491-5
+	.M
+	Allied Health Personnel/*; Electric Countershock/*; Emergencies; Emergency Medical Technicians/*; Human; Prognosis; Recurrence; Support, U.S. Gov't, P.H.S.; Time Factors; Transportation of Patients; Ventricular Fibrillation/*TH.
+	.T
+	Refibrillation managed by EMT-Ds: incidence and outcome without paramedic back-up.
+	.P
+	JOURNAL ARTICLE.
+	.W
+	Some patients converted from ventricular fibrillation to organized rhythms by defibrillation-trained ambulance technicians (EMT-Ds) will refibrillate before hospital arrival. The authors analyzed 271 cases of ventricular fibrillation managed by EMT-Ds working without paramedic back-up. Of 111 patients initially converted to organized rhythms, 19 (17%) refibrillated, 11 (58%) of whom were reconverted to perfusing rhythms, including nine of 11 (82%) who had spontaneous pulses prior to refibrillation. Among patients initially converted to organized rhythms, hospital admission rates were lower for patients who refibrillated than for patients who did not (53% versus 76%, P = NS), although discharge rates were virtually identical (37% and 35%, respectively). Scene-to-hospital transport times were not predictively associated with either the frequency of refibrillation or patient outcome. Defibrillation-trained EMTs can effectively manage refibrillation with additional shocks and are not at a significant disadvantage when paramedic back-up is not available.
+	.A
+	Stults KR; Brown DD.
+
+	*/
+	
+	/*
+	private String I;
+	private String U;
+	private String S;
+	private String M;
+	private String T;
+	private String P;
+	private String W;
+	private String A;
+
+	public Document() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+	public String getI() {
+		return I;
+	}
+
+	public void setI(String i) {
+		I = i;
+	}
+
+	public String getU() {
+		return U;
+	}
+
+	public void setU(String u) {
+		U = u;
+	}
+
+	public String getS() {
+		return S;
+	}
+
+	public void setS(String s) {
+		S = s;
+	}
+
+	public String getM() {
+		return M;
+	}
+
+	public void setM(String m) {
+		M = m;
+	}
+
+	public String getT() {
+		return T;
+	}
+
+	public void setT(String t) {
+		T = t;
+	}
+
+	public String getP() {
+		return P;
+	}
+
+	public void setP(String p) {
+		P = p;
+	}
+
+	public String getW() {
+		return W;
+	}
+
+	public void setW(String w) {
+		W = w;
+	}
+
+	public String getA() {
+		return A;
+	}
+
+	public void setA(String a) {
+		A = a;
+	}
+*/
+}
