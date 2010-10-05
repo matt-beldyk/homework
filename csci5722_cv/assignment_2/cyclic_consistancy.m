d6a6373528ffd5a329443531aabe8ed78b6e4672
matt beldyk <beldyk@Sloth.(none)>
2010-10-04 23:30:05 -0600

    should have inported this earlier

---
+++ csci5722_cv/assignment_2/cyclic_consistancy.m
@@ -0,0 +1,22 @@
+
+% this function makes sure that a result is contained in 
+% both a forwards search, and a backwards search
+function [union] = cyclic_consistancy(A,B)
+    k = 1;
+    
+    % flip around my results so I can compare if they are equal
+    C(:,1) = B(:,3);
+    C(:,2) = B(:,4);
+    C(:,3) = B(:,1);
+    C(:,4) = B(:,2);
+
+    for i = 1:size(A)
+        for j = 1:size(C)
+            if(A(i,:) == C(j,:))
+                union(k,:) = A(i,:);
+                k = k +1;
+            end
+        end
+    end
+    
+end
