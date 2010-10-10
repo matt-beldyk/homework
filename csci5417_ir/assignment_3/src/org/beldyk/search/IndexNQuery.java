01717da819cad48c99e579421f66de0d20d29528
matt beldyk <beldyk@Sloth.(none)>
2010-10-09 21:49:57 -0600

    now with my custom stoplist

--- csci5417_ir/assignment_3/src/org/beldyk/search/IndexNQuery.java
+++ csci5417_ir/assignment_3/src/org/beldyk/search/IndexNQuery.java
@@ -17,6 +17,7 @@ public class IndexNQuery {
 	public static void main(String[] args) throws LockObtainFailedException, IOException, Exception {
 		// TODO Auto-generated method stub
 		Dials dial = new Dials();
+		System.out.println("running...");
 		dial.setAnalyz(new MattsAnalyzer());
 		Indexer dex = new Indexer(dial);
 		Queryer qyr = new Queryer(dial);
