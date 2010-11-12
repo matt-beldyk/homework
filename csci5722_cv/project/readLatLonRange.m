4582a2a0d5c354cec214fd61035e067f90bb33ea
matt beldyk <beldyk@Sloth.(none)>
2010-11-11 23:23:17 -0700

    properly aligning dems

--- csci5722_cv/project/readLatLonRange.m
+++ csci5722_cv/project/readLatLonRange.m
@@ -1,19 +1,26 @@
 function [dem] = readLatLonRange(minLat, maxLat, minLon, maxLon)
 
-    latSize = maxLat - minLat +1;
-    lonSize = maxLon - minLon +1;
+    latSize = maxLat - minLat + 1
+    lonSize = maxLon - minLon + 1
     
     demSize = 3601;
     dem = zeros(latSize*demSize, lonSize*demSize);
     
-    for lat = minLat:maxLat
-        for lon = minLon: maxLon
-            latStart = (lat - minLat) * demSize
-            lonStart = (lon - minLon) * demSize
-            latEnd = (lat - minLat + 1) * demSize
-            lonEnd = (lon - minLon + 1) * demSize
+    for i = 0:latSize -1
+        for j = 0:lonSize -1
+            i
+            j
+            latStart = i * demSize + 1
+            lonStart = j * demSize + 1
+            latEnd = (i + 1) * demSize
+            lonEnd = (j + 1) * demSize
+            %latStart = (lat - minLat) * demSize + 1
+            %lonStart = (lon - minLon) * demSize + 1
+            %latEnd = (lat - minLat + 1) * demSize 
+            %lonEnd = (lon - minLon + 1) * demSize 
             
-            dem(latStart:latEnd, lonStart:lonEnd) = findSRTMData(lat,lon);
+            dem(latStart:latEnd, lonStart:lonEnd) = flipdim(findSRTMData(minLat + i,minLon + j),2)';
+            %dem(latStart:latEnd, lonStart:lonEnd) = findSRTMData(minLat + i,minLon + j)';
         end
     end
 end
