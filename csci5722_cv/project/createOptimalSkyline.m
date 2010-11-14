43c9efc1896eb3fa6d83ba041f126fa3587d124c
matt beldyk <beldyk@Sloth.(none)>
2010-11-13 21:24:25 -0700

    with proper direction finding

--- csci5722_cv/project/createOptimalSkyline.m
+++ csci5722_cv/project/createOptimalSkyline.m
@@ -2,6 +2,17 @@ function [img] = createOptimalSkyline(lat, lon, distanceToLook, width, height)
     latDist = vdist(lat, lon, lat+1, lon)
     lonDist = vdist(lat, lon, lat, lon +1)
     
-    dem = findSRTMData(floor(lat), floor(lon), 30);
-    surf(dem);
+    dem = findSRTMData(floor(lat), floor(lon), 50);
+    %surf(dem);
+    maxLoc = max(max(dem))
+    img = zeros(width, height)
+    [sizeDem, tmp] = size(dem)
+    for i = 1:sizeDem
+        for j = 1:sizeDem
+            [mlat, mlon] = demIndex2LatLon(i,j, sizeDem, lat, lon);
+            sprintf('%d,%d = %3.4f,%3.4f', i,j, mlat, mlon)
+            [dist, heading] = findHeading(lat, lon, mlat, mlon,latDist, lonDist);
+            sprintf('Dist=%f Heading=%f\n', dist, heading)
+        end
+    end
 end
--- csci5722_cv/project/findHeading.m
+++ csci5722_cv/project/findHeading.m
@@ -1,5 +1,5 @@
 function [dist, heading] = findHeading(myLat, myLon, awayLat, awayLon, distPerLat, distPerLon)
-    heading = arctan((awayLat - myLat)/(awayLon - myLon))
+    heading = atan((awayLat - myLat)/(awayLon - myLon))
     
     % Yes, I'm cheating this is straight line distance from a cheaty
     % version of lat lon distance.  I'm assuming it doesn't matter on this
