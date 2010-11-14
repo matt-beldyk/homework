0da517504ef72d05dc3556641295fa37d8b50883
matt beldyk <beldyk@Sloth.(none)>
2010-11-13 21:57:38 -0700

    proper alignmnet

--- csci5722_cv/project/createOptimalSkyline.m
+++ csci5722_cv/project/createOptimalSkyline.m
@@ -1,18 +1,27 @@
 function [img] = createOptimalSkyline(lat, lon, distanceToLook, width, height)
     latDist = vdist(lat, lon, lat+1, lon)
     lonDist = vdist(lat, lon, lat, lon +1)
-    
-    dem = findSRTMData(floor(lat), floor(lon), 50);
+   
+    cornerLat = floor(lat);
+    cornerLon = floor(lon);
+    dem = findSRTMData(cornerLat, cornerLon, 50);
+   
     %surf(dem);
     maxLoc = max(max(dem))
-    img = zeros(width, height)
+    img = zeros(width, height);
     [sizeDem, tmp] = size(dem)
+    headings = zeros(sizeDem,sizeDem);
     for i = 1:sizeDem
         for j = 1:sizeDem
-            [mlat, mlon] = demIndex2LatLon(i,j, sizeDem, lat, lon);
-            sprintf('%d,%d = %3.4f,%3.4f', i,j, mlat, mlon)
+            [mlat, mlon] = demIndex2LatLon(i,j, sizeDem, cornerLat, cornerLon);
+           % sprintf('%d,%d = %3.4f,%3.4f', i,j, mlat, mlon);
             [dist, heading] = findHeading(lat, lon, mlat, mlon,latDist, lonDist);
-            sprintf('Dist=%f Heading=%f\n', dist, heading)
+           % sprintf('Dist=%f Heading=%f\n', dist, heading)
+            headings(i,j) = heading;
+            
         end
     end
+    
+    maxHeading = max(max(headings))
+    minHeading = min(min(headings))
 end
--- csci5722_cv/project/findHeading.m
+++ csci5722_cv/project/findHeading.m
@@ -1,11 +1,29 @@
 function [dist, heading] = findHeading(myLat, myLon, awayLat, awayLon, distPerLat, distPerLon)
-    heading = atan((awayLat - myLat)/(awayLon - myLon))
+
+    latDist = distPerLat * (awayLat - myLat);
+    lonDist = distPerLon * (awayLon - myLon);
+    dist = sqrt(latDist*latDist + lonDist*lonDist);
+    heading = atan2(latDist, lonDist);
     
+    
+    %if 0
+    %if(awayLat == myLat && awayLon == myLon)
+    %    dist = 0;
+    %    heading = 0;
+%     end
+%     if latDist >=0
+%             
+%         dist = sqrt(latDist*latDist + lonDist*lonDist);
+%         heading = asin(lonDist/dist);
+%     end
+%     if latDist < 0 
+%         dist = sqrt(latDist*latDist + lonDist*lonDist);
+%         heading = -asin(lonDist/dist) + pi;
+%     end
+%     end
     % Yes, I'm cheating this is straight line distance from a cheaty
     % version of lat lon distance.  I'm assuming it doesn't matter on this
     % scale and wgs84 great circle distance is an *expensive* computation
     
-    latDist = distPerLat * (awayLat - myLat)
-    lonDist = distPerLon * (awayLon - myLon)
-    dist = sqrt(latDist*latDist + lonDist*lonDist)
+
 end
