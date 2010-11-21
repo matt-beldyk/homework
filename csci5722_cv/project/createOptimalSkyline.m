cecc70ef8710975233a26f10f55761fd82eb2fee
matt beldyk <beldyk@Sloth.(none)>
2010-11-20 23:00:02 -0700

    clipping things that are too close

--- csci5722_cv/project/createOptimalSkyline.m
+++ csci5722_cv/project/createOptimalSkyline.m
@@ -1,12 +1,12 @@
 function [img] = createOptimalSkyline(lat, lon, distanceToLook, width, height, decimationLevel)
 latDist = vdist(lat, lon, lat+1, lon)
 lonDist = vdist(lat, lon, lat, lon +1)
+tooCloseCutoff = 25000; % 15 KM
 
 %if( ~decimationLevel)
 %   decimationLevel = 50;
 %end
-cornerLat = floor(lat);
-cornerLon = floor(lon);
+
 img = zeros(height, width,3);
 
 maxElevation = 0;
@@ -26,72 +26,45 @@ for cornerLat = floor(lat) -distanceToLook:floor(lat)+distanceToLook
             end
             for j = 1:sizeDem
                 
-                %sprintf('minHeading=%f, maxHeading=%f', minHeading, maxHeading)
                 
-                % sprintf('%d,%d = %3.4f,%3.4f', i,j, mlat, mlon);
-                %[dist, heading] = findHeading(lat, lon, mlat, mlon,latDist, lonDist);
-                % [lDist, leftHeading] = findHeading(leftLat, leftLon, mlat, mlon, latDist, lonDist);
-                %[rDist, rightHeading] = findHeading(rightLat, rightLon, mlat, mlon, latDist, lonDist);
-                
-                % sprintf('Dist=%f Heading=%f\n', dist, heading)
-                %  headings(i,j) = heading;
-                %  dists(i,j) = dist;
-
                 %sprintf('i=%d j=%d, dropoff=%.2f dist=%.2f', i,j,dropoff,dist)
                 
                 [minHeading, maxHeading, distance] = calculateHeadingWindow(i,j,lat, lon, cornerLat, cornerLon, latDist, lonDist, decimationLevel, sizeDem);
-                dropoff = calculateDistDropoff(distance);
-                % mark up the skyline pic
-               % skyLinePoint = floor((heading + pi) * width/(2*pi));
-                leftSkyLinePoint =  floor((minHeading + pi) * width/(2*pi));
-                rightSkyLinePoint = floor((maxHeading + pi) * width/(2*pi));
-                
-                % oi vey, matlab indexing starts at 1
-               % if skyLinePoint ==0
-               %     skyLinePoint = 1;
-               % end
-               
-               
-                if leftSkyLinePoint == 0
-                    leftSkyLinePoint = 1;
-                end
-                if rightSkyLinePoint == 0
-                    rightSkyLinePoint = 1;
-                end
-                
-                
-                pointElevation = floor(dem(i,j) + dropoff);
-                
-                % clip to my actual margins, don't run past
-                if pointElevation > height
-                    if maxElevation < pointElevation
-                        maxElevation = pointElevation;
-                    end
-                    pointElevation = height -1;
-                end
-                
-                
-                % while k > 0 && ( not(img(k,skyLinePoint, 3)) || (img(k,skyLinePoint, 3) > dist))
-                if pointElevation> 0
-                    sprintf('distance=%f should be %i wide',distance, rightSkyLinePoint - leftSkyLinePoint +1);
-                    for k = leftSkyLinePoint:rightSkyLinePoint
-                        if not(img(pointElevation,k, 3)) || (img(pointElevation,k, 3) > distance)
-                            %   for k = 1:dem(i,j)
-                            %  if not(img(k,skyLinePoint, 3)) || (img(k,skyLinePoint, 3) > dist)
-                            %sprintf('points should be %i wide', rightSkyLinePoint - leftSkyLinePoint +1)
-                            
-                            %  sprintf('pointElevation=%f, k=%f', pointElevation, k)
-                            img(pointElevation,k ,:) = [lat, lon, distance]';
+                if distance > tooCloseCutoff
+                    dropoff = calculateDistDropoff(distance);
+                    
+                    % mark up the skyline pic
+                    leftSkyLinePoint =  heading2Index(minHeading, width);
+                    rightSkyLinePoint = heading2Index(maxHeading, width);
+                  
+                    pointElevation = floor(dem(i,j) + dropoff);
+                    
+                    % clip to my actual margins, don't run past
+                    if pointElevation > height
+                        if maxElevation < pointElevation
+                            maxElevation = pointElevation;
                         end
-                        % end
-                        % end
-                        % k = k -1;
+                        pointElevation = height -1;
+                    end
+                    
+                    
+                    
+                    if pointElevation> 0
+                        sprintf('distance=%f should be %i wide',distance, rightSkyLinePoint - leftSkyLinePoint +1);
+                        for k = leftSkyLinePoint:rightSkyLinePoint
+                            if not(img(pointElevation,k, 3)) || (img(pointElevation,k, 3) > distance)
+                                
+                                %sprintf('points should be %i wide', rightSkyLinePoint - leftSkyLinePoint +1)
+                                
+                                img(pointElevation,k ,:) = [lat, lon, distance]';
+                            end
+                            
+                        end
                     end
                 end
             end
         end
-        % image(img)
-        % img(:,:,3)
+        
         demCounter = demCounter +1;
     end
 end
@@ -103,4 +76,14 @@ end
 %maxDistance = max(max(dists))
 maxElevation
 %minElevation = min(min(dem))
+end
+
+
+% takes a heading of -pi to pi and turns it into a horizontal index
+% for the picute
+function [dex] = heading2Index(heading, width)
+    dex =  floor((heading + pi) * width/(2*pi));
+    if dex==0
+        dex =1;
+    end
 end
