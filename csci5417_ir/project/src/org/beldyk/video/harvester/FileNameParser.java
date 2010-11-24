ff230326b88882cc9977494dbfefcadcda9d0753
matt beldyk <beldyk@Sloth.(none)>
2010-11-23 23:32:32 -0700

    parseing filenames for my informations desired

--- csci5417_ir/project/src/org/beldyk/video/harvester/FileNameParser.java
+++ csci5417_ir/project/src/org/beldyk/video/harvester/FileNameParser.java
@@ -1,29 +1,61 @@
 package org.beldyk.video.harvester;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class FileNameParser {
-	private String fileName;
-	
-	public FileNameParser(String fName){
-		this.fileName = fName;
+	private Collection<PattMatcher> pattMatch;
+
+	public FileNameParser(String pattFile) throws FileNotFoundException{
+		pattMatch = getPattsFromFile(pattFile);
 	}
 
-	public void setFileName(String fileName) {
-		this.fileName = fileName;
+	/**
+	 * creates a collection of pattern matchers from a file that describes how 
+	 * they should be defined
+	 * 
+	 * @param pattFile
+	 * @return
+	 * @throws FileNotFoundException
+	 */
+	private Collection<PattMatcher> getPattsFromFile(String pattFile) throws FileNotFoundException{
+		Collection<PattMatcher> patts = new ArrayList<PattMatcher>();
+		Scanner scanner = new Scanner(new File(pattFile),"UTF-8");
+		try {
+			while (scanner.hasNextLine()){
+				String line = scanner.nextLine();
+				if(! ( 
+						line.startsWith("#")||
+						line.length() <1
+						)){
+					patts.add(pattLine2Matcher(line));
+				}
+			}
+		}finally{
+			scanner.close();
+		}
+
+		return patts;
 	}
 
-	public String getFileName() {
-		return fileName;
+	protected PattMatcher pattLine2Matcher(String line){
+		return new PattMatcher(line.split("##"));
 	}
-	
 
-	public String getSeriesName(){
-		Pattern patt = Pattern.compile("([\\w_]+) \\d+");
-		Matcher matt = patt.matcher(this.fileName);
-		matt.find();
-		System.err.println(matt.group(1));
-		return matt.group(1);
+	public Map<String, String> parseFileName(String fileName){
+		for(PattMatcher pm: pattMatch){
+			Map<String, String> tags = pm.parseString(fileName);
+			if(!(tags == null)){ // I've found a match
+				return tags;
+			}
+			
+		}
+		return null;
 	}
 }
--- csci5417_ir/project/src/org/beldyk/video/harvester/PattMatcher.java
+++ csci5417_ir/project/src/org/beldyk/video/harvester/PattMatcher.java
@@ -8,7 +8,7 @@ import java.util.regex.Pattern;
 public class PattMatcher {
 	private Map <String, Integer> tags;
 	private Pattern patt;
-	
+
 
 	public PattMatcher(String pattern, String [] tgs){
 		tags = new HashMap<String, Integer>();
@@ -18,21 +18,32 @@ public class PattMatcher {
 			tags.put(tgs[i], i+1);
 		}
 	}
+	public PattMatcher(String[] tgs) {
+		tags = new HashMap<String, Integer>();
+
+		patt = Pattern.compile(tgs[0]);
+		for(Integer i = 1; i<tgs.length; i++){
+			tags.put(tgs[i], i);
+		}
+	}
 	public Map<String, String> parseString(String str){
 		Matcher mat = patt.matcher(str);
-		mat.find();
-		
-/*		if(!mat.matches()){
+		if (mat.find()){
+
+			/*		if(!mat.matches()){
 			System.err.printf("pattern:'%s' does not match string '%s'\n", patt.toString(), str);
 			return null;
 		}
-		*/
-		Map<String, String> pTags = new HashMap<String, String>();
-		for(String tag: tags.keySet()){
-			System.err.println(tag+":"+tags.get(tag));
-			pTags.put(tag, mat.group(tags.get(tag)));
+			 */
+			Map<String, String> pTags = new HashMap<String, String>();
+			for(String tag: tags.keySet()){
+				System.err.println(tag+":"+tags.get(tag));
+				pTags.put(tag, mat.group(tags.get(tag)));
+			}
+
+			return pTags;
+		}else{
+			return null;
 		}
-		
-		return pTags;
 	}
 }
