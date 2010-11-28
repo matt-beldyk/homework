6785750c82be17cc2f3b09dfc1beef748303028b
matt beldyk <beldyk@Sloth.(none)>
2010-11-28 16:11:35 -0700

    calculating proper features really fast

--- csci5722_cv/assignment_2/find_features_and_mappings.m
+++ csci5722_cv/assignment_2/find_features_and_mappings.m
@@ -2,47 +2,50 @@
 
 % This is the main function, basically give it a path to the example
 % dataset provided (the *.ppm images of haybales.) and provides an 
-function [xy_mappings] = find_features_and_mappings(pth, threshold)    
+function [xy_mappings] = find_features_and_mappings(pth, maxDist, threshold)    
     begin_time = cputime();
     images = read_files(pth);
     [w,h, count_images] = size(images)
     showy = zeros(w,h,3,count_images);
     
-   % count_images = 3;
+    %count_images = 3;
 
     xy_mappings = cell(1,count_images - 1);
     feat_vects = cell(1,count_images);
 
     for i = 1:count_images
-        this_image_is = i
+        this_image_is = i;
 
         % convolute image with all the filters
         convoluted = do_convolutions(images(:,:,i));
+        sprintf('convolved image %d', i)
         
         % find all my features for this image
-        features = find_features(convoluted);
+        features = find_features(convoluted, threshold);
+        sprintf('features found %d', i)
         
         % translate these features into more easily mathish vectors
         feat_vects{1,i} = make_feature_vect(convoluted, features);
-
+        sprintf('feature vects made %d', i)
 
         % find the mappings from the last image to this one
         if(i > 1)
          %   xy_mappings{1,i} = find_xy_mappings(feat_vects{1,i-1}, feat_vects{1,i});
-            xy_mappings{1,i} = map_via_grid(feat_vects{1,i-1}, feat_vects{1,i}, w, h, threshold);
+            xy_mappings{1,i} = map_via_grid(feat_vects{1,i-1}, feat_vects{1,i}, w, h, maxDist);
         end
         
         % mark up an image for my display purposes
         showy(:,:,:,i) = mark_up_picture(feat_vects{1,i}, images(:,:,i));
     end
     end_time = cputime();
-    total_time = end_time - begin_time
+    total_time = end_time - begin_time;
     
+    sprintf('total compute time: %f seconds', total_time)
     % uncomment for debugging
-    save('code_state_oct_12.mat');
+    save('code_state_nov_27.mat');
    
    % Display my results
-  %  show_results(showy, xy_mappings);
+    show_results(showy, xy_mappings);
 
 
 end
@@ -87,47 +90,6 @@ function [feat_vect] = make_feature_vect(conv_mat, features)
     end
 end
 
-
-% This is where the magic happens, basically, I take my 3-d matrix of
-% convolutions and do a search for all the local minimas and maximas.  I
-% look at the various planes on each size to see if point N is bigger or
-% smaller than both, and iterate over all three dimensions.  Truely, if I
-% were aiming for speed, it might make sense to do some benchmarks and
-% lower the number of comparisons I'm doing.  But the real slowness in the
-% program isn't this function and it's likely that Matlab is doing a fine
-% job of loading in memory and that memory load is the big hit here
-function [features] = find_features(conv_mat)
-features = zeros(size(conv_mat));
-[w,h,n] = size(features);
-for x = 2:w-1
-  %  x
-    for y = 2:h-1
-        for z = 2:n-1
-            
-
-            %top bottom
-            is_max = (conv_mat(x,y,z) > max(max(conv_mat(x-1:x+1, y-1, z-1:z+1)))) && (conv_mat(x,y,z) > max(max(conv_mat(x-1:x+1, y+1, z-1:z+1))));
-            is_min = (conv_mat(x,y,z) < min(min(conv_mat(x-1:x+1, y-1, z-1:z+1)))) && (conv_mat(x,y,z) < min(min(conv_mat(x-1:x+1, y+1, z-1:z+1))));
-            
-            %left right
-            is_max = is_max && (conv_mat(x,y,z) > max(max(conv_mat(x-1, y-1:y+1, z-1:z+1)))) && (conv_mat(x,y,z) > max(max(conv_mat(x+1, y-1:y+1, z-1:z+1))));
-            is_min = is_min && (conv_mat(x,y,z) < min(min(conv_mat(x-1, y-1:y+1, z-1:z+1)))) && (conv_mat(x,y,z) < min(min(conv_mat(x+1, y-1:y+1, z-1:z+1))));
-        
-            %back front
-            is_max = is_max && (conv_mat(x,y,z) > max(max(conv_mat(x-1:x+1, y-1:y+1, z-1)))) && (conv_mat(x,y,z) > max(max(conv_mat(x-1:x+1, y-1:y+1, z+1))));
-            is_min = is_min && (conv_mat(x,y,z) < min(min(conv_mat(x-1:x+1, y-1:y+1, z-1)))) && (conv_mat(x,y,z) < min(min(conv_mat(x-1:x+1, y-1:y+1, z+1))));
-            
-            if is_max 
-                features(x,y,z) = 1;
-            end
-            if is_min
-                features(x,y,z) = -1;
-            end
-        end
-    end
-end
-
-end
 
 
 % for every filter size, convolve the image and store the results in 
--- csci5722_cv/assignment_3/map_via_grid.m
+++ csci5722_cv/assignment_3/map_via_grid.m
@@ -6,13 +6,13 @@
 % looking for coorespondances via a threshold
 
 
-function [mappings] = map_via_grid(A,B, w, h, threshold)
+function [mappings] = map_via_grid(A,B, w, h, maxDist)
     Ma = map_features_grid(A, w, h);
     Mb = map_features_grid(B, w, h);
     %forward_matches = find_matches(Ma, Mb, w, h, threshold);
     %back_matches = find_matches(Mb, Ma, w, h, threshold);
     %mappings = cyclic_consistancy(forward_matches, back_matches);
-    mappings = find_matches(Ma, Mb, w, h, threshold);
+    mappings = find_matches(Ma, Mb, w, h, maxDist);
 end
 
 % puts the features into a grid
@@ -26,33 +26,33 @@ end
 
 % searches the two grids looking for cooresponances between 
 % the two of them
-function [matches] = find_matches(Ma, Mb, w, h, threshold)
+function [matches] = find_matches(Ma, Mb, w, h, maxDist)
 g = 1;
 
 for i = 1:w
     for j = 1:h
         if(Ma{i,j})
             
-            if i>threshold
-                left = i-threshold;
+            if i>maxDist
+                left = i-maxDist;
             else
                 left = 1;
             end
             
-            if i + threshold < w
-                right = i+threshold;
+            if i + maxDist < w
+                right = i+maxDist;
             else
                 right = w;
             end
             
-            if j>threshold
-                up = j-threshold;
+            if j>maxDist
+                up = j-maxDist;
             else
                 up = 1;
             end
             
-            if j + threshold < h
-                down = j+threshold;
+            if j + maxDist < h
+                down = j+maxDist;
             else
                 down = h;
             end
