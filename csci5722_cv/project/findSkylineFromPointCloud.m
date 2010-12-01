3702f5528a0846dfdc041be4eaf25de6d2830f4f
matt beldyk <beldyk@Sloth.(none)>
2010-11-30 23:31:57 -0700

    initial import of holder to match skylines

--- csci5722_cv/project/findSkylineFromPointCloud.m
+++ csci5722_cv/project/findSkylineFromPointCloud.m
@@ -1,12 +1,17 @@
-function [skyline] = findSkylineFromPointCloud(cloud)
+function [skyline, angleSkyline] = findSkylineFromPointCloud(cloud)
     [w,h, tmp] = size(cloud);
     skyline = zeros(w,1);
+    angleSkyline = zeros(w,1);
+    
     for i = 1:h
         j = w;
         while j > 1&& ~cloud(j,i,3) 
             j = j-1;
         end
+        
+        angle = pi *(j/h) - pi/2;
         skyline(i) = j;
+        angleSkyline(i) = angle;
 
 
     end
